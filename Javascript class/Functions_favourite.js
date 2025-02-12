bfb1e6f777a67b7b8ef8879847546e87c974a34d
iamprashanthks <ksaiprashanth05@gmail.com>
2025-02-12 12:51:16 +0530

    functions

---
+++ Javascript class/Functions_favourite.js
@@ -0,0 +1,13 @@
+// Favourite
+
+var fActor = "Tom cruise";
+var fplayer = "Messy";
+var fMovie = "Sonic 3";
+
+function favourite() {
+  console.log("My Favourite Actor is: " + fActor);
+  console.log("My Favourite Actor is: " + fplayer);
+  console.log("My Favourite Actor is: " + fMovie);
+}
+
+favourite();
---
+++ Javascript class/Price x Count.js
@@ -0,0 +1,8 @@
+// Price * Quantity
+
+var fruitName = "Apple";
+var price = 10;
+var count = 5;
+var total = price * count;
+console.log(fruitName);
+console.log(total);
