4f9e95f224497275001a0ae35581f3b8d0cfa2f6
iamprashanthks <ksaiprashanth05@gmail.com>
2025-02-12 12:58:10 +0530

    vowel or consonent

---
+++ Javascript class/vowel or consonent.js
@@ -0,0 +1,15 @@
+var character = "k";
+
+if (character == "a") {
+  console.log("This is a vowel");
+} else if (character == "e") {
+  console.log("This is vowel");
+} else if (character == "i") {
+  console.log("This is vowel");
+} else if (character == "o") {
+  console.log("This is vowel");
+} else if (character == "u") {
+  console.log("This is vowel");
+} else {
+  console.log("This is Consonant");
+}
