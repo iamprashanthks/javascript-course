9020582ffd1cfdd32aaf1b911f9cba871a74bb7b
iamprashanthks <ksaiprashanth05@gmail.com>
2025-02-12 12:55:24 +0530

    if_else

---
+++ Javascript class/if else.js
@@ -0,0 +1,41 @@
+// 1
+var raining = false;
+if (raining) {
+  console.log("Take an umbrella.");
+} else {
+  console.log("Enjoy the sunshine.");
+}
+
+// 2
+var homework = false;
+
+if (homework) {
+  console.log("Great job!");
+} else {
+  console.log("Finish your homework before playing.");
+}
+
+// 3
+var cookies = true;
+if (cookies) {
+  console.log("Would you like a cookie?");
+} else {
+  console.log("Time to bake more cookies.");
+}
+
+// 4
+var brush = true;
+
+if (brush) {
+  console.log("Your teeth are clean.");
+} else {
+  console.log("Go brush your teeth now.");
+}
+
+// 5
+var birthday = true;
+if (birthday) {
+  console.log("Happy birthday! Enjoy your special day!");
+} else {
+  console.log("Have a great day!");
+}
