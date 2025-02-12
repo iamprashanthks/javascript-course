e76c13becdce1d863e54a661c75de4c82c90ac23
iamprashanthks <ksaiprashanth05@gmail.com>
2025-02-12 12:56:49 +0530

    what season is this

---
+++ Javascript class/what season is this.js
@@ -0,0 +1,17 @@
+var season = "summer";
+
+if (season == "spring") {
+  console.log("Enjoy the blooming flowers");
+}
+
+if (season == "summer") {
+  console.log("Have fun in the sun");
+}
+
+if (season == "autumn") {
+  console.log("Admire the colorfull leaves");
+}
+
+if (season == "winter") {
+  console.log("Bundle up & stay warm");
+}
